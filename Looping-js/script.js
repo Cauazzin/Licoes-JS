1e5659508e0fea73a346790ded96f30a0d76b65f
Cauazzin <cauafsantos17@gmail.com>
2025-04-07 18:54:55 -0300

    Add files via upload

---
+++ Looping-js/script.js
@@ -0,0 +1,59 @@
+/* =====ESCOLHA CASO===== */
+
+/* var diaSemana = Number(prompt("Digite um dia da semana 1 a 7"))
+
+
+switch (diaSemana) {
+    case 1:
+        alert("Domingo")
+        break
+    case 2: 
+        alert("Segunda")
+        break
+    case 3:
+        alert("Terça")
+        break
+    case 4:
+        alert("Quarta")
+        break
+    case 5:
+        alert("Quinta")
+        break
+    case 6:
+        alert("Sexta")
+        break
+    case 7:
+        alert("Sábado")
+        break
+    default:
+        alert("Não existe este dia da semana [ERRO]")
+        break
+} */
+
+
+
+
+/* =====ARRAY []===== */
+
+/* var carros = ["Celta", "Civic", "Brasilia", "Corsa", "HB20"]
+
+alert(carros[carros.length -1]) */
+
+
+
+/* =====OBJETOS===== */
+
+/* 
+var pessoa = {
+    nome: "Cauã",
+    idade: 18,
+    tel:"(11)934980000",
+    bigode:false,
+    barba:false,
+}
+
+alert(`Nome: ${pessoa.nome}
+idade: ${pessoa.idade}
+tel: ${pessoa.tel}`)
+ */
+
